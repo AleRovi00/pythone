326d68fd0e8aff3ad23e2c58b39a836635ba40a2
AleRovi00 <alerovi04@gmail.com>
2025-03-18 16:17:47 +0100

    soluzioni

--- mesi.py
+++ mesi.py
@@ -15,4 +15,4 @@ mese = int(input("indica un mese: "))
 p = (mese - 1) * 9
 print (MESI[p] + MESI[p+1] + MESI[p+2] + MESI[p+3] + MESI[p+4] + MESI[p+5] 
        + MESI[p+6] + MESI[p+7] + MESI[p+8])
-print (MESI[p:p+9])
+#print (MESI[p:p+9])
