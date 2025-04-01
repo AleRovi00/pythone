1d050de88326593e9aafb63092496ddb97c21a4e
AleRovi00 <alerovi04@gmail.com>
2025-04-01 13:01:10 +0200

    else

--- imposta.py
+++ imposta.py
@@ -9,11 +9,12 @@ scal2= 50000
 if reddito <= scal1 :
  irpef= reddito * IVA23
 
-elif reddito > scal1 and reddito <= scal2 :
+else:
+ if reddito > scal1 and reddito <= scal2 :
   irpef= (scal1 * IVA23) + (reddito-scal1)*IVA35 
- 
-elif reddito > scal2:
-  irpef= (scal1 * IVA23) + (scal2-scal1)*IVA35 + (reddito-scal2)*IVA43
+ else:
+  if reddito > scal2:
+   irpef= (scal1 * IVA23) + (scal2-scal1)*IVA35 + (reddito-scal2)*IVA43
 
 print ("la tua imposta è: ", irpef)
 
