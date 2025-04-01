58da0f35d1b9e7607dd735b00d1667411c8aea6a
AleRovi00 <alerovi04@gmail.com>
2025-04-01 10:16:04 +0200

    nome

---
+++ carte_da_pocker.py
@@ -0,0 +1,15 @@
+cuori_bytes = b"\xE2\x99\xA1"
+cuori = cuori_bytes.decode()
+
+quadri_bytes = b"\xE2\x99\xA2"
+quadri = quadri_bytes.decode()
+
+fiori_bytes = b"\xE2\x99\xA7"
+fiori = fiori_bytes.decode()
+
+picche_bytes = b"\xE2\x99\xA4"
+picche = picche_bytes.decode()
+
+print (cuori + " " + quadri + " " + fiori + " " + picche)
+
+
