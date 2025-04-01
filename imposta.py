593062cbeb99c5686708d11cbe9adeea469448c8
AleRovi00 <alerovi04@gmail.com>
2025-04-01 13:16:49 +0200

    ind

--- imposta.py
+++ imposta.py
@@ -1,21 +1,21 @@
-redditostr = input ("dimmi il reddito: ")
-reddito= float(redditostr)
 IVA23 = 0.23
 IVA35 = 0.35
 IVA43 = 0.43
-irpef= 0.0
-scal1= 28000
-scal2= 50000
-if reddito <= scal1 :
- irpef= reddito * IVA23
+irpef = 0.0
+scal1 = 28000
+scal2 = 50000
+redditostr = input("dimmi il reddito: ")
+reddito = float(redditostr)
+if reddito <= scal1:
+    irpef = reddito * IVA23
 
 else:
- if reddito > scal1 and reddito <= scal2 :
-  irpef= (scal1 * IVA23) + (reddito-scal1)*IVA35 
- else:
-  if reddito > scal2:
-   irpef= (scal1 * IVA23) + (scal2-scal1)*IVA35 + (reddito-scal2)*IVA43
+    if reddito > scal1 and reddito <= scal2:
+        irpef = (scal1 * IVA23) + (reddito - scal1) * IVA35
+    else:
+        if reddito > scal2:
+            irpef = (
+                (scal1 * IVA23) + (scal2 - scal1) * IVA35 + (reddito - scal2) * IVA43
+            )
 
-print ("la tua imposta è: ", irpef)
-
-  
+print("la tua imposta è: ", irpef)
