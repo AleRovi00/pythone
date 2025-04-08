9c69ef1856737603074c19a67e77778f1da58d02
AleRovi00 <alerovi04@gmail.com>
2025-04-08 10:52:55 +0200

    vuoto

--- massimo.py
+++ massimo.py
@@ -4,5 +4,8 @@ lista = []
 input_utente = input("Inserisci un numero (o 'q' per terminare): ")
 while input_utente != 'q':
        lista.append(int(input_utente))
-       input_utente = input("Inserisci un numero (o 'q' per terminare): ")  
-       print("Il valore massimo della lista è:", max(lista))
+       input_utente = input("Inserisci un numero (o 'q' per terminare): ")
+if lista: 
+    print("Il valore massimo della lista è:", max(lista))
+else:
+    print("La lista è vuota.")
