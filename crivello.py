0c56cb69544290816745d16af4531f32874bded9
AleRovi00 <alerovi04@gmail.com>
2025-04-09 13:05:49 +0200

    n

--- crivello.py
+++ crivello.py
@@ -1,12 +1,13 @@
 
 def crea_insieme_numeri(N):
-    return set(range(1, N + 1))
+    return set(range(2, N + 1))
 
 # Esempio di utilizzo
-numeri = 20
+numeri = 100
 insieme = crea_insieme_numeri(numeri)
 #print(insieme)
 # Rimuovere i numeri multipli da 2 a N dall'insieme
 for i in range(2, int(numeri ** 1/2) + 1):
     insieme = {num for num in insieme if num % i != 0 or num == i}
 print(insieme)
+print(len(insieme))
