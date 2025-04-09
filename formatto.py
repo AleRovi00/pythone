711e8f7afe8a1aa3bd88a99631bb3ee455a6bde3
AleRovi00 <alerovi04@gmail.com>
2025-04-09 11:53:40 +0200

    sol

--- formatto.py
+++ formatto.py
@@ -1,7 +1,11 @@
 
 reciproci = [1 / x for x in range(1, 11)]
-print(reciproci)
-print([float(f"{x:.8f}") for x in reciproci])
+#print(reciproci)
+for x in reciproci:
+    print(f"{x:.8f}")
 
+#reciproci = [1 / x for x in range(1, 11)]
+#reciproci_con_8_decimali = [round(x, 8) for x in reciproci]
+#print(reciproci_con_8_decimali)
 
     
