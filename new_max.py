6626a4c996d124dbf17069f7393e60dbad93ded2
AleRovi00 <alerovi04@gmail.com>
2025-04-16 10:47:41 +0200

    max

---
+++ new_max.py
@@ -0,0 +1,27 @@
+def mediaFileScelto():
+    filename = input("Inserisci il nome del file da leggere: ")
+    daLeggere = open(filename, "r")
+    righe = daLeggere.readlines()
+    daLeggere.close()
+    i = 0
+    somma = 0
+    lista=[]
+
+    for riga in righe:
+        somma += float(riga)
+        i += 1
+    media = (somma / i)
+    if i == 0:
+        print("La somma è zero")
+    print("la media è: ", media)
+
+    for riga in righe:
+        lista.append(float(riga))
+    if lista:
+        print("Il valore massimo della lista è:", max(lista))
+        
+    
+mediaFileScelto()
+
+
+
