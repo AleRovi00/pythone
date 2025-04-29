c39d2eec8318584a9ce58cc70c4cdc5ca2f5e3fd
AleRovi00 <alerovi04@gmail.com>
2025-04-29 10:58:34 +0200

    spig

---
+++ spigolatura.py
@@ -0,0 +1,14 @@
+import urllib.request
+url = "https://www.acmilan.com/it"
+risultato = urllib.request.urlopen(url)
+theBytes = risultato.read()
+text = theBytes.decode()
+import bs4
+doc = bs4.BeautifulSoup(text)
+print(doc.prettify())
+
+def naviga2(tag, indent) :
+    print(indent + tag.name.upper())
+    for stag in tag.contents:
+        if type(stag) == bs4.element.Tag :
+            naviga2(stag, indent + " ")
